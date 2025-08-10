06504cb32d8263deb7d854b482fc4de017a10310
AkbarOktaviadi89 <Akbaroktaviadi89@gmail.com>
2025-08-10 15:18:38 +0700

    Feat: Add Livewire Component and Pages

--- routes/web.php
+++ routes/web.php
@@ -16,40 +16,40 @@ use App\Livewire\Pages\PaymentSuccessPage;
 use App\Livewire\Pages\ScanPage;
 use Livewire\Livewire;
 
-Route::middleware(CheckTableNumber::class)->group(function () {
-    Route::get('/', HomePage::class)->name('home');
-    Route::get('/post', AllFoodPage::class)->name('product.index');
-    Route::get('/food/favorite', FavoritePage::class)->name('product.favorite');
-    Route::get('/food/promo', PromoPage::class)->name('product.promo');
-    Route::get('/food/{id}', DetailPage::class)->name('product.detail');
+// Route::middleware(CheckTableNumber::class)->group(function () {
+//     Route::get('/', HomePage::class)->name('home');
+//     Route::get('/post', AllFoodPage::class)->name('product.index');
+//     Route::get('/food/favorite', FavoritePage::class)->name('product.favorite');
+//     Route::get('/food/promo', PromoPage::class)->name('product.promo');
+//     Route::get('/food/{id}', DetailPage::class)->name('product.detail');
     
-});
+// });
 
-Route::middleware(CheckTableNumber::class)->controller(TransactionController::class)->group(function () { 
-    Route::get('/cart', CartPage::class)->name('payment.cart');
-    Route::get('/checkout', CheckoutPage::class)->name('payment.checkout');
+// Route::middleware(CheckTableNumber::class)->controller(TransactionController::class)->group(function () { 
+//     Route::get('/cart', CartPage::class)->name('payment.cart');
+//     Route::get('/checkout', CheckoutPage::class)->name('payment.checkout');
 
-    //proses pembayaran
-    Route::middleware('throttle:10,1')->post('/payment','handlePayment')->name('payment');
-    Route::get('/payment', function(){
-        abort(404);
-    });
+//     //proses pembayaran
+//     Route::middleware('throttle:10,1')->post('/payment','handlePayment')->name('payment');
+//     Route::get('/payment', function(){
+//         abort(404);
+//     });
 
-    //status pembayaran
-    Route::get('/payment/status/{id}', 'paymentStatus')->name('payment.status');
-    Route::get('/payment/success', PaymentSuccessPage::class)->name('payment.success');
-    Route::get('/payment/failure', PaymentFailurePage::class)->name('payment.failure');
-});
+//     //status pembayaran
+//     Route::get('/payment/status/{id}', 'paymentStatus')->name('payment.status');
+//     Route::get('/payment/success', PaymentSuccessPage::class)->name('payment.success');
+//     Route::get('/payment/failure', PaymentFailurePage::class)->name('payment.failure');
+// });
 
-Route::post('/payment/webhook', [TransactionController::class, 'handleWebhook'])->name('payment.webhook');
+// Route::post('/payment/webhook', [TransactionController::class, 'handleWebhook'])->name('payment.webhook');
 
-Route::controller(QRController::class)->group(function () {
-    Route::post('/store-qr-result', 'storeResult')->name('product.scan.store');
-    //scanner
-    Route::get('/scan', ScanPage::class)->name('product.scan');
-    Route::get('/{tableNumber}','checkCode')->name('product.scan.table');
+// Route::controller(QRController::class)->group(function () {
+//     Route::post('/store-qr-result', 'storeResult')->name('product.scan.store');
+//     //scanner
+//     Route::get('/scan', ScanPage::class)->name('product.scan');
+//     Route::get('/{tableNumber}','checkCode')->name('product.scan.table');
 
-});
+// });
 
 Route::get('/', function () {
     return view('welcome');
